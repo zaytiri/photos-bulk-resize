ea1fac7755e94df20618bda4b80c80e5458d6773
zay <64491586+zaytiri@users.noreply.github.com>
2023-04-04 22:36:40 +0100

    update setup, requirements and changelog
    updated changelof file; update setup.py; added package requirements;

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,7 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Fixed
 - Removed
 
-## [1.0.0] - yyyy-mm-dd
+## [1.0.0] - 2023-04-05
 
 ### Added
 - First release on PyPI
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import pathlib
 
-from version.progsettings import get_version
+from phulize.version.progsettings import get_version
 
 here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
@@ -9,15 +9,15 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 version = get_version()
 
 setup(
-    name="python-template",
+    name="phulize",
     version=version,
-    description="",
+    description="A python CLI command tool to resize photos while conserving folder hierarchy and preserving original ones in a different folder.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/zaytiri/python-template",
+    url="https://github.com/zaytiri/photos-bulk-resize",
     project_urls={
-        'GitHub': 'https://github.com/zaytiri/python-template',
-        'Changelog': 'https://github.com/zaytiri/python-template/blob/main/CHANGELOG.md',
+        'GitHub': 'https://github.com/zaytiri/photos-bulk-resize',
+        'Changelog': 'https://github.com/zaytiri/photos-bulk-resize/blob/main/CHANGELOG.md',
     },
     author="zaytiri",
     classifiers=[
@@ -27,16 +27,18 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
     ],
-    keywords="keywords, separated, by, commas",
-    package_data={'python-template': ['progsettings.yaml']},
-    packages=["python-template", "python-template.subfolder1", "python-template.subfolder2"],
+    keywords="photos, image, processing, resize, reduce, cli, folder, hierarchy, bulk",
+    package_data={'phulize': ['progsettings.yaml']},
+    packages=["phulize", "phulize.settings", "phulize.version", "phulize.utils"],
     python_requires=">=3.10.6",
     install_requires=[
-      "PyYAML~=6.0",
+        "PyYAML~=6.0",
+        "margument>=1.0.3",
+        "Pillow~=9.5.0",
     ],
     entry_points={
         "console_scripts": [
-            "python-template=python-template:app.main",
+            "phulize=phulize:app.main",
         ],
     }
 )
