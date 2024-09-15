a7f2b1a1d93e1d16566ea44bfede485cbc008027
zay <catarinafbalves@gmail.com>
2024-09-15 16:17:43 +0200

    fix getting version file

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_version():
     if getattr(sys, 'frozen', False):
         path = os.path.join(sys._MEIPASS, "files/progsettings.yaml")
     else:
-        path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'progsettings.yaml')
+        path = os.path.join(os.path.dirname(__file__), 'phulize', 'version', 'progsettings.yaml')
 
     with open(path, 'r') as settings_file:
         settings = yaml.safe_load(settings_file)['prog'.upper()]
